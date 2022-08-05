e3c8a31f2fbde4dcf48200a7261420313eb9e57a
winqus <67459222+winqus@users.noreply.github.com>
2022-08-05 18:59:28 +0300

    Implemented answering

--- trivia.js
+++ trivia.js
@@ -1,6 +1,7 @@
 // https://opentdb.com/api.php?amount=10&type=multiple
 
 var triviaObject = {};
+var userAnswers = {};
 
 function ClearCards() {
     $("#allCards").empty();
@@ -8,7 +9,7 @@ function ClearCards() {
 
 function AddCard(id) {
     $("#allCards").append (`
-        <li onclick="LoadQuestion(${id});" class="list-group-item d-flex gap-3 align-items-center listCard">
+        <li onclick="LoadQuestion(${id});" id="q${id}" class="list-group-item d-flex gap-3 align-items-center listCard">
         <div class="profileCircle rounded-circle flex-shrink-0">${id+1}</div>
         <div class="d-flex w-100"><h4>Question ${id+1}</h2></div>
         </li>
@@ -24,6 +25,66 @@ function SetTitle(bubbleText, titleText) {
 function SetInqueryMessage(newQuery) {
     $("#inqueryPlaceHolder").empty().append(newQuery);
 
+}
+
+function GetAnswerCount () {
+    var answeredCount = 0;
+    for(let i = 0; i < triviaObject.results.length; i++) {
+        if(userAnswers[i].answered)
+        {
+            answeredCount++;
+        }
+    }
+    return answeredCount;
+}
+
+function GetCorrectAnswerCount () {
+    var answeredCount = 0;
+    for(let i = 0; i < triviaObject.results.length; i++) {
+        if(userAnswers[i].answered && userAnswers[i].correct)
+        {
+            answeredCount++;
+        }
+    }
+    return answeredCount;
+}
+
+function CheckGameEnd() {
+    if(GetAnswerCount() >= 10) {
+        SetInqueryMessage(`    
+        <div class="card-body">
+        <h5 class="card-title">You correctly answered:</h5>
+        <h3 class="card-title">${GetCorrectAnswerCount()}/${triviaObject.results.length}</h3>
+        <a href="" class="btn btn-primary">Try again?</a>
+        </div>
+    `);
+    }
+}
+
+function CheckSelectedAnswer(thisObj, id, answer) {
+    if(userAnswers[id].answered)
+        return;
+    userAnswers[id].answered = true;
+
+    correctAnswer = triviaObject.results[id].correct_answer.replaceAll(' ', '_');
+
+    $(thisObj).removeClass("btn-secondary");
+    
+    if($(thisObj).text().replaceAll(' ', '_') == correctAnswer) {
+        userAnswers[id].correct = true;
+        $(thisObj).addClass("btn-success");
+        $(`#q${id}`).addClass("goodAnswerQ");
+
+        CheckGameEnd();
+        return;
+    }
+    userAnswers[id].correct = false;
+    $(thisObj).addClass("btn-danger");
+    $(`#q${id}`).addClass("badAnswerQ");
+    $(`#${id}_${correctAnswer}`).removeClass("btn-secondary");
+    $(`#${id}_${correctAnswer}`).addClass("btn-success");
+
+    CheckGameEnd();
 }
 
 function Shuffle(array) {
@@ -48,9 +109,14 @@ function LoadQuestion(id) {
     SetTitle(`${id+1}`, `Question ${id+1}`);
     let trivia = triviaObject.results[id];
     let answerButtons = [];
-    answerButtons.push(`<a href="#" class="m-2 btn btn-secondary">${trivia.correct_answer}</a>`);
+    if(userAnswers[id].answered)
+        answerButtons.push(`<a href="#" onclick="CheckSelectedAnswer(\$(this), ${id}, '${trivia.correct_answer.replaceAll(' ', '_')}');" id="${id}_${trivia.correct_answer.replaceAll(' ', '_')}" class="m-2 btn btn-success">${trivia.correct_answer}</a>`);
+    else  answerButtons.push(`<a href="#" onclick="CheckSelectedAnswer(\$(this), ${id}, '${trivia.correct_answer.replaceAll(' ', '_')}');" id="${id}_${trivia.correct_answer.replaceAll(' ', '_')}" class="m-2 btn btn-secondary">${trivia.correct_answer}</a>`);
+    //else answerButtons.push(`<a href="#" onclick="CheckSelectedAnswer(\$(this), ${id}, '${trivia.correct_answer.replaceAll(' ', '_')}');" id="${id}_${trivia.correct_answer.replaceAll(' ', '_')}" class="m-2 btn btn-secondary">${trivia.correct_answer}</a>`);
     trivia.incorrect_answers.forEach(incorrectAnswer => {
-        answerButtons.push(`<a href="#" class="m-2 btn btn-secondary">${incorrectAnswer}</a>`);
+        if(userAnswers[id].answered)
+            answerButtons.push(`<a href="#" onclick="CheckSelectedAnswer(\$(this), ${id}, '${incorrectAnswer.replaceAll(' ', '_')}');" id="${id}_${incorrectAnswer.replaceAll(' ', '_')}" class="m-2 btn btn-danger">${incorrectAnswer}</a>`);
+        else answerButtons.push(`<a href="#" onclick="CheckSelectedAnswer(\$(this), ${id}, '${incorrectAnswer.replaceAll(' ', '_')}');" id="${id}_${incorrectAnswer.replaceAll(' ', '_')}" class="m-2 btn btn-secondary">${incorrectAnswer}</a>`);
     });
     let shuffledButtons = Shuffle(answerButtons);
     buttonsHtml = ``;
@@ -75,23 +141,24 @@ function GetTrivia() {
         let len = triviaObject.results.length;
         for(let i = 0; i < len; i++) {
             AddCard(i);
+            userAnswers[i] = {
+                "answered" : false,
+                "correct" : false
+            };
         }
       }
-
-      );
-
-    
+    );
 }
 
 $(document).ready( () => {
     ClearCards();
     SetTitle("", "It's Trivia Time!");
+    GetTrivia();
     SetInqueryMessage(`<div class="card-body">
     <h5 class="card-title">Welcome to random Trivia!</h5>
     <p class="card-text">You'll be provided with several questions to answer. Try to get as many correct as possible!</p>
-    <a href="#" class="btn btn-primary">Start!</a>
+    <a href="#" onclick="LoadQuestion(0);" class="btn btn-primary">Start!</a>
     </div>`);
-    GetTrivia();
 
     
 });
